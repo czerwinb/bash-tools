6fd15bd6ee45d7118f2acba232c61cccedf460f0
Bartosz Czerwiński <bartosz@czerwinb.pl>
2017-04-03 13:22:59 +0200

    Adding command line options for culprits extractor

--- Git-Reporting-Tools/generate-html-report-on-remote-branches.sh
+++ Git-Reporting-Tools/generate-html-report-on-remote-branches.sh
@@ -3,12 +3,15 @@
 BRANCH_AGE_THRESHOLD_DAYS=14
 
 BASEPATH="$(dirname $0)"
+BASENAME="$(basename $0)"
 TEMPLATES_PATH="${BASEPATH}/html"
 
 REPORT_HEADER_TEMPLATE="${TEMPLATES_PATH}/report-header.template"
 REPORT_FOOTER_TEMPLATE="${TEMPLATES_PATH}/report-footer.template"
 REPORT_ROW_TEMPLATE="${TEMPLATES_PATH}/report-row.template"
+
 REPORT_FILE="${BASEPATH}/report.html"
+CULPRITS_FILE="${BASEPATH}/culprits.csv"
 
 source ${BASEPATH}/utils/git-utils.sh
 
@@ -35,9 +38,9 @@ function generate_report_for() {
     done
 }
 
-function prepare_report_file() {
-	local report_file="${1}"
-	cat /dev/null > ${report_file}
+function truncate_file() {
+	local file="${1}"
+	cat /dev/null > ${file}
 }
 
 function append_report_header() {
@@ -82,17 +85,72 @@ function append_report_row() {
 		>> ${report_file}
 }
 
+function generate_culprits_list_for() {
+	local culprits_file="${1}"
+	local branches="${2}"
+    local separator=""
+
+    for branch in ${branches}; do
+        load_BRANCH_METADATA_for "${branch}"
+        
+        local last_commit_timestamp="${BRANCH_METADATA[2]}"
+        local branch_age_days=$((($(date +%s) - $last_commit_timestamp) / 60 / 60 / 24))
+        local author_name="${BRANCH_METADATA[3]}"
+        local author_email="${BRANCH_METADATA[4]}"
+        
+        if [ "${branch_age_days}" -gt "${BRANCH_AGE_THRESHOLD_DAYS}" ]; then
+            printf "%s%s <%s>" \
+                "${separator}" \
+                "${author_name}" \
+                "${author_email}" \
+                >> ${culprits_file}
+            separator="; "
+        fi
+    done
+}
+
+function parse_input_arguments() {
+    for i in "$@"; do
+        case "$i" in
+            --report-file=*)
+            REPORT_FILE="${i#*=}"
+            shift
+            ;;
+            --extract-culprits)
+            EXTRACT_CULPRITS=1
+            shift
+            ;;
+            --culprits-file=*)
+            CULPRITS_FILE="${i#*=}"
+            EXTRACT_CULPRITS=1
+            shift
+            ;;
+            --help|*)
+            echo "Usage: ${BASENAME} [--report-file=<path>] [--extract-culprits] [--culprits-file=<path>]"
+            exit 0
+            ;;
+        esac
+    done
+}
+
 #
 # MAIN
 #
 exit_if_not_inside_valid_git_repository
+parse_input_arguments "$@"
 
 echo "Generating HTML report to ${REPORT_FILE}..."
 
-prepare_report_file "${REPORT_FILE}"
+truncate_file "${REPORT_FILE}"
 append_report_header "${REPORT_FILE}"
 load_UNMERGED_REMOTE_BRANCHES
 generate_report_for "${REPORT_FILE}" "${UNMERGED_REMOTE_BRANCHES}"
 append_report_footer "${REPORT_FILE}"
+
+if [ "${EXTRACT_CULPRITS}" ]; then
+    echo "Generating culprits list to ${CULPRITS_FILE}..."
+    truncate_file "${CULPRITS_FILE}"
+    generate_culprits_list_for "${CULPRITS_FILE}" "${UNMERGED_REMOTE_BRANCHES}"
+fi
 
 echo "...done!"
